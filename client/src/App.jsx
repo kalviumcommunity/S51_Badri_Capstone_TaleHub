72d43e5bc820f2c5ca42fdd3c7b12de5524fa6ce
Badri <ram.r@kalvium.community>
2024-03-13 14:05:40 +0530

    added front-end basic code

---
+++ client/src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from "react";
+import "./App.css";
+
+function App() {
+  
+
+  return (
+    <div className="appJSX">
+        <div className="navBar">Navbar</div>
+        <div className="route">Actual Content</div>
+    </div>
+  );
+}
+
+export default App
