95d933b475673df2321b25339588afa78b7a2ff9
Badri <ram.r@kalvium.community>
2024-04-04 15:12:23 +0530

    made the login and sign up functional

--- routes/login.js
+++ routes/login.js
@@ -1,52 +1,39 @@
 const express = require("express");
 const router = express.Router();
 const jwt = require("jsonwebtoken");
-const passport = require("passport");
-const LocalStrategy = require("passport-local").Strategy;
 const Profile = require("../models/user.model");
 require("dotenv").config();
 
-// Local Strategy for email login
-passport.use(
-  "local-login",
-  new LocalStrategy(
-    { usernameField: "email" },
-    async (email, password, done) => {
-      try {
-        const user = await Profile.findOne({ email: email });
-        if (!user) {
-          return done(null, false, { message: "Invalid email or password" });
-        }
-        return done(null, user);
-      } catch (error) {
-        return done(error);
-      }
+router.post("/signin", async (req, res) => {
+  try {
+    const { email, password } = req.body;
+    const user = await Profile.findOne({ email });
+
+    if (!user || user.password !== password) {
+      return res.status(401).json({ message: "Invalid email or password" });
     }
-  )
-);
 
-router.post(
-  "/signin",
-  passport.authenticate("local-login", { session: false }),
-  (req, res) => {
-    const token = jwt.sign({ user: req.user }, process.env.JWT_SECRET);
-    console.log(req.user);
+    const token = jwt.sign({ user }, process.env.JWT_SECRET);
     res.json({ token });
+  } catch (error) {
+    console.error("Error:", error);
+    res.status(500).json({ message: "Internal server error" });
   }
-);
+});
 
-// Route for email sign-up
 router.post("/signup", async (req, res) => {
   try {
-    const existingUser = await Profile.findOne({ email: req.body.email });
+    const { name, email, password } = req.body;
+    const existingUser = await Profile.findOne({ email });
+
     if (existingUser) {
       return res.status(400).json({ message: "Email is already registered." });
     }
 
     const newUser = new Profile({
-      name: req.body.name,
-      email: req.body.email,
-      password: req.body.password,
+      name,
+      email,
+      password,
       mangaCart: [],
       booksCart: [],
     });
