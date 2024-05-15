e7ec7df435ebf7abeeac8f1eb7a12a8c48869c3d
heybadrinath <heybadrinath@gmail.com>
2024-05-15 12:13:18 +0530

    added new component for AI aearch

--- client/src/AllRoutes.jsx
+++ client/src/AllRoutes.jsx
@@ -5,7 +5,7 @@ import BooksPage from "./components/BooksPage";
 import MangaPage from "./components/MangaPage";
 import CartPage from "./components/CartPage";
 import StoryTeller from "./components/StoryTeller";
-
+import BookFinder from "./components/BookFinder";
 function AllRoutes({ onLoginClick, userData, setUserData }) {
   return (
     <Routes>
@@ -50,6 +50,7 @@ function AllRoutes({ onLoginClick, userData, setUserData }) {
         }
       ></Route>
       <Route path="/story" element={<StoryTeller />}></Route>
+      <Route path="/bookFinder" element={<BookFinder />}></Route>
     </Routes>
   );
 }
